987069841b08ead50909e918aa4b3c8bd3924f00
Владислав Коренюк <vladisl.korenuyk@x5.ru>
2026-08-10 03:53:56 +0300

    v0.1.3 - Иконка, удаление терминала, правки CI/CD

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(target_os = "windows", windows_subsystem = "windows")]
+
 use crate::gui::Application;
 use iced::Font;
 
@@ -5,9 +7,25 @@ pub mod export;
 pub mod gui;
 pub mod points;
 
+fn application_icon() -> Option<iced::window::Icon> {
+    let icon = image::load_from_memory_with_format(
+        include_bytes!("../packaging/icons/app-icon-window.png"),
+        image::ImageFormat::Png,
+    )
+    .ok()?
+    .into_rgba8();
+    let (width, height) = icon.dimensions();
+
+    iced::window::icon::from_rgba(icon.into_raw(), width, height).ok()
+}
+
 fn main() -> iced::Result {
     iced::application(Application::new, Application::update, Application::view)
         .title("Точки ЭЭГ")
+        .window(iced::window::Settings {
+            icon: application_icon(),
+            ..iced::window::Settings::default()
+        })
         .window_size((1040.0, 680.0))
         .resizable(false)
         .centered()
